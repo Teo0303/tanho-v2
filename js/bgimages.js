01b889b8111ffeb5e17849c28929e93159359699
Jonibekov Azizbek <jonibekov97@gmail.com>
2019-11-14 20:30:59 +0500

    Another change

--- js/bgimages.js
+++ js/bgimages.js
@@ -1,8 +1,15 @@
-fetch("http://localhost:1337/bgimages")
-  .then(res => res.json())
-  .then(data => {
-    let about = document.getElementById("about");
-    let banner = document.getElementById("home");
-    about.style.backgroundImage = `url('./img/Tanho/${data[0].bg_image.name}')`;
-    banner.style.backgroundImage = `url('./img/${data[1].bg_image.name}')`;
-  });
+const api2 = `http://localhost:1337`;
+
+fetch(api2 + 'bgimages').then((res) => res.json()).then((data) => {
+	let about = document.getElementById('about');
+	let banner = document.getElementById('home');
+	about.style.backgroundImage = `url('${api2 + data[0].bg_image.url}')`;
+	banner.style.backgroundImage = `url('${api2 + data[1].bg_image.url}')`;
+});
+
+fetch(api2 + '/statics').then((res) => res.json()).then((data) => {
+	aboutDesc = document.getElementById('about-desc');
+	aboutTitle = document.getElementById('about-title');
+	aboutDesc.innerHTML = data[0].description;
+	aboutTitle.innerHTML = data[0].title;
+});
